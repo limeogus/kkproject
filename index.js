8d291a5e99b22a0a12927ee2a7578961d6d86c66
eogus <limeogus@naver.com>
2020-10-12 14:16:46 +0900

    hellow world 예제

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-const fs = require('fs')
+const http = require('http');
 
-const file = fs.readFile('text.txt', {
-    encoding : 'utf8'
-}, (err, data) => console.log(data)
-) //테스트 파일입니다.
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    if(req.url ==='/'){
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Hello World');
+    }else if(req.url==='/users'){
+        const users = [{name:'Alice'}, {name:'beck'}
+        ]
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify(users));
+    }
+
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
