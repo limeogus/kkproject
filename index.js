c38a6ad497fa4a8d1981f2c893d03251469d90b2
eogus <limeogus@naver.com>
2020-10-24 11:05:11 +0900

    서버실행 분기 및 쿼리 스트링 실습

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const express = require('express')
 const logger = require('morgan')
 const app = express()
-const users = [{name: 'Alice'}]
+const users = [
+    {id : 1 , name: 'Alice'},
+    {id : 2 , name : 'Bek'},
+    {id : 3 , name : 'Chris'}
+]
 
 app.get('/', (req,res) => res.send('Hello world!'))
-app.get('/users', (req, res) => res.json(users))
+app.get('/users', (req, res) => {
+    
+    req.query.limit = req.query.limit || 10 // 없을 경우 defalut = 10
+    const limit = parseInt(req.query.limit, 10)
+    
+    // todo
+    //if(limit =="NaN"){ 
+    if(Number.isNaN(limit)){
+        res.status(400).end()
 
-//app.listen(3000, () => console.log('running'))
+    }else{
+
+        res.json(users.slice(0, limit))
+    }
+    //console.log(limit)
+
+})
+
+app.listen(3000, () => console.log('running'))
 
 
 module.exports = app
--- index.spec.js
+++ index.spec.js
@@ -4,12 +4,43 @@ const request = require('supertest')
 const app = require('./index')
 
 describe('GET /users', () => {
+    //done 함수 : mocha에서 지원
+    //중요한 포인트 마다 테스트 코드를 만드는 것이 중요한 것
+    describe('성공', ()=>{
+        it('배열을 반환한다.', (done)=>{
+                request(app)
+                .get('/users')
+                .end((err, res) =>{
+                    //console.log(res.body)
+                    //1.should를 이용한 배열 확인
+                    res.body.should.be.instanceof(Array)
+                    
+                    //2.should를 이용한 배열 확인
+                    res.body.forEach(user => {
+                        user.should.have.property('name')
+                    })
 
-    //it : mocha 함수
-    it('배열을 반환한다.', ()=>{
-        //node 기본모듈 assert
-        //assert.equal(1,0)   
-        (1).should.equal(1)
+                    done() //test 종료
+                })
+            })
+            
+            it('최대 limit 갯수만큼 응답한다', (done) =>{
+                request(app)
+                .get('/users?limit=2')
+                .end((err, res) =>{
+                    res.body.should.have.lengthOf(2)
+                    done()
+                })
+            })
 
+        })
+
+        describe('실패', ()=>{
+            it('limit이 정수가 아니면 400을 응답한다.', done =>{
+                request(app)
+                .get('/users?limit=one')
+                .expect(400)
+                .end(done)
+            })
+        })
     })
-})
